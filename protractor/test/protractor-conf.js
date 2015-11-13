68df9985fe288fb232e8106051651e1145dba60e
ameliaburger <aburger@uw.edu>
2015-11-12 16:24:27 -0800

    protractor

--- protractor/test/protractor-conf.js
+++ protractor/test/protractor-conf.js
@@ -1,5 +1,8 @@
+// set exports.config equal to an object like the selenium address you should be using
 exports.config = {
     seleniumAddress: 'http://localhost:4444/wd/hub',
     specs: ['*-spec.js'],
-    rootElement: 'body'
+    rootElement: 'body' // optional - used to find ng-app on whatever element name you put here
+    // can use an element selector like here or a css selector
+    // rootElement defaults to body
 };
