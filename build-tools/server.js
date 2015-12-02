8bcf32b1daaf8c88d8a8bf809471ced02bc270eb
ameliaburger <aburger@uw.edu>
2015-12-01 17:20:35 -0800

    learn gulp, final note

--- build-tools/server.js
+++ build-tools/server.js
@@ -3,3 +3,5 @@ require('./Gulpfile.js');
 gulp.start('default');
 
 // use npm start at command line to make gulp work without it being installed
+
+// after cloning down a repo use npm install and then npm start
