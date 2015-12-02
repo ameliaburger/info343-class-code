68e82d658a63dd2df1a7c6b3d7b216820ca05ec8
ameliaburger <aburger@uw.edu>
2015-12-01 17:19:16 -0800

    learn grunt and gulp

---
+++ build-tools/Gulpfile.js
@@ -0,0 +1,49 @@
+var gulp = require('gulp');
+var connect = require('gulp-connect');
+var sass = require('gulp-sass');
+var minifyHTML = require('gulp-minify-html');
+var uglify = require('gulp-uglify');
+
+gulp.task('connect', function() {
+    connect.server({
+        root: 'dawg-coffee',
+        livereload: true
+    })
+});
+
+gulp.task('sass', function() {
+    gulp.src('dawg-coffee/scss/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('dawg-coffee/css'))
+        .pipe(connect.reload());
+});
+
+gulp.task('sass:watch', function() {
+   gulp.watch('dawg-coffee/scss/*.scss', ['sass']);
+});
+
+
+gulp.task('uglify', function() {
+    gulp.src('dawg-coffee/js/*.js')
+        .pipe(uglify())
+        .pipe(gulp.dest('dist/js'));
+});
+
+gulp.task('minify', function() {
+    gulp.src('dawg-coffee/*.html')
+        .pipe(minifyHTML())
+        .pipe(gulp.dest('dist/'));
+});
+
+gulp.task('watch', function() {
+    gulp.watch('dawg-coffee/scss/*.scss', ['sass']);
+    gulp.watch('dawg-coffee/js/*.js', ['uglify']);
+    gulp.watch('dawg-coffee/*.html', ['minify']);
+});
+
+gulp.task('copy', function() {
+    gulp.src('dawg-coffee/img/*')
+        .pipe(gulp.dest('dist/img/'));
+});
+
+gulp.task('default', ['sass', 'sass:watch', 'minify', 'uglify', 'connect']);
---
+++ build-tools/server.js
@@ -0,0 +1,5 @@
+var gulp = require('gulp');
+require('./Gulpfile.js');
+gulp.start('default');
+
+// use npm start at command line to make gulp work without it being installed
